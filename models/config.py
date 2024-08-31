ea46a2e0ff808c5cf949d5c13ab09c1027b05265
wibucrypto <56129456+wibucrypto2201@users.noreply.github.com>
2024-08-31 16:43:15 +0700

    Add files via upload

---
+++ models/config.py
@@ -0,0 +1,26 @@
+from better_proxy import Proxy
+from pydantic import BaseModel, PositiveInt, ConfigDict
+
+
+class Account(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    email: str
+    password: str
+    imap_server: str = ""
+    proxy: Proxy
+
+
+class Config(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    class DelayBeforeStart(BaseModel):
+        min: int
+        max: int
+
+    accounts_to_farm: list[Account] = []
+    threads: PositiveInt
+    imap_settings: dict[str, str]
+
+    keepalive_interval: PositiveInt
+    module: str = ""
